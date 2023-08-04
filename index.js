e5218c099780be59d1040aad62573043e268a303
Nazmul66 <hnazmul748@gmail.com>
2023-08-05 04:36:39 +0600

    delete api done

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
        res.send(result);
    })
 
+   // components -> JobList has been fetch the data
+   app.delete('/jobDelete/:id', async(req, res) =>{
+     const id = req.params.id;
+     const query = {_id : new ObjectId(id)} 
+     const result = await JobApplied.deleteOne(query);
+     res.send(result)
+  })
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
